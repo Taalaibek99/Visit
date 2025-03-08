f6a4e39995d30c4315a6b6502b3570ac5dfa7285
Taalaibek99 <Begjan421@gmail.com>
2025-03-08 21:29:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 document.getElementById('submitBtn').addEventListener('click', function () {
     const uid = document.getElementById('uidInput').value;
+
     if (!uid) {
         alert('Please enter a UID.');
         return;
     }
 
     // API'ге сурам жөнөтүү
-    fetch(`https://freefire-virusteam.vercel.app/visit?key=$Taalaimodz444&uid=${uid}&sl=$50`) // API URL'ин өзгөртүңүз
+    const apiUrl = `https://freefire-virusteam.vercel.app/visit?key=$Taalaimodz444&uid=${uid}&sl=$50`;
+
+    fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
